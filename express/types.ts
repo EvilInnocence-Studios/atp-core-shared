95b5f44144f6f1a5751d1f8b03ecdcddae81036d
Andrew Wittrock <andy@wittrock.us>
2025-01-20 21:55:37 -0600

    Update query types

--- express/types.ts
+++ express/types.ts
@@ -5,5 +5,8 @@ export type NewObj<T extends {id:number}> = Omit<T, "id">;
 export declare type Headers = Index<string>;
 
 export type Params = Index<string>;
-export type Query = Paging & Index<string | string[]>;
+export type QuerySingleValue = string | number | boolean | null;
+export type QueryArrayValue = string[] | number[] | boolean[] | null[];
+export type QueryValue = QuerySingleValue | QueryArrayValue;
+export type Query = Paging & Index<QueryValue>;
 export declare type Paging = {offset?:number, perPage?:number};
